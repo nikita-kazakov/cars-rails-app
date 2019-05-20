8fa988d399b05318f07d41456c8132e338d534a7
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-05-20 14:00:00 +0100

    MChanged Controller

--- cars/app/controllers/cars_controller.rb
+++ cars/app/controllers/cars_controller.rb
@@ -8,4 +8,25 @@ class CarsController < ApplicationController
     @car = Car.find(params[:id])
   end
 
+  def edit
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def update
+    @car = Car.find(params[:id])
+    @car.update(params.require(:car).permit(:make, :model))
+    redirect_to @car
+
+    #Below is the quickest way to update but with FORBIDDEN FAILURE.
+    #fail
+    #@car = Car.find(params[:id])
+    #@car.update(params[:car])
+    #redirect_to @car
+
+  end
+
+
 end
