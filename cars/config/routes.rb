fb62a79315da34d7ccd20b9d2236a565061c7ffa
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-05-19 23:18:48 -0400

    Bootstrap HTML Research

--- cars/config/routes.rb
+++ cars/config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'cars#index'
+  get '/cars/new' => 'cars#new', as: 'new_car'
+  get '/cars/:id/edit' => "cars#edit", as: 'edit_car'
+  get '/cars/:id' => "cars#show", as: 'car'
+
+  patch '/cars/:id' => "cars#update"
+
+  #resources :cars
+
+
 end
