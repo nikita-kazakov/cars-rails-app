0b3671431560b26eba60044d2aa110bc94eec59b
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-05-23 13:35:17 -0400

    image helper + seeds.db generated

--- cars/app/helpers/cars_helper.rb
+++ cars/app/helpers/cars_helper.rb
@@ -1,2 +1,13 @@
 module CarsHelper
+
+  def car_image(car)
+
+    if car.image.blank?
+      image_tag("car_placeholder.png", class:"img-fluid d-block")
+    else
+      image_tag(car.image, class:"img-fluid d-block")
+    end
+
+  end
+
 end
--- cars/db/seeds.rb
+++ cars/db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Car.create!([
+  {make: "Hyundai", model: "Kona", transmission: "Auto", year: "2019-01-01", cylinders: 8, mpg: "54.0", price: "25080.0", image: "2019_hyundai_kona.jpg"},
+  {make: "Honda", model: "Civic", transmission: "Auto", year: "2017-01-01", cylinders: 6, mpg: "19.0", price: "32600.0", image: "honda_civic.jpg"},
+  {make: "Kia", model: "Forte", transmission: "Auto", year: nil, cylinders: 4, mpg: "18.0", price: "35000.0", image: "kia_forte.jpg"},
+  {make: "Baggle", model: "Hags", transmission: "manual", year: nil, cylinders: 10, mpg: "50.0", price: "0.0", image: ""}
+])
